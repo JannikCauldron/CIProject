587b6cbe90bdf670015efcfad4a9b26d90bff6ae
jannik.kessler <jannik.kessler@informatik.hs-fulda.de>
2020-12-29 16:11:56 +0100

    #3 refactor: added addPattern and regex matching.

--- src/main/java/MultiMath_Math/Calculator.java
+++ src/main/java/MultiMath_Math/Calculator.java
@@ -1,8 +1,20 @@
 package MultiMath_Math;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
+    private Pattern addPattern = Pattern.compile("[0-9]\\s\\+\\s[0-9]");
+
     public int operate(String operation) {
-        return 5;
+        int result = 0;
+        Matcher addMatcher = addPattern.matcher(operation);
+        if (addMatcher.find()) {
+            String matchedOperation = addMatcher.group();
+            result = Integer.parseInt(matchedOperation.substring(0,1)) + Integer.parseInt(matchedOperation.substring(4,5));
+        }
+
+        return result;
     }
 }
