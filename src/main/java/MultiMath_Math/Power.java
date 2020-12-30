b68725a3c03c87f4d3df3c53180c9660934afe7d
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2020-12-30 14:06:48 +0100

    #7 added: exponentiate two double

---
+++ src/main/java/MultiMath_Math/Power.java
@@ -0,0 +1,7 @@
+package MultiMath_Math;
+
+public class Power {
+    public int exponentiate(String expo) {
+        return 9;
+    }
+}
---
+++ src/test/java/MultiMath_Math/PowerTest.java
@@ -0,0 +1,27 @@
+package MultiMath_Math;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class PowerTest {
+    Power pow;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        pow = new Power();
+    }
+
+    @Test
+    public void exponentiateTest() throws Exception {
+        //Arrange
+        String operation = "3 ^ 2";
+        double expected = Math.pow(3, 2);
+        //Act
+        double result = pow.exponentiate(operation);
+        //Assert
+        assertThat("wrong power", result, CoreMatchers.equalTo(expected));
+    }
+}
