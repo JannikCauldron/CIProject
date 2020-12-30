573a42d959ef8c289d30d4f0521a8e87f0ccefc1
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2020-12-30 17:18:13 +0100

    #7 refactor: pattern matcher added

--- src/main/java/MultiMath_Math/Power.java
+++ src/main/java/MultiMath_Math/Power.java
@@ -1,7 +1,18 @@
 package MultiMath_Math;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Power {
+
+    private Pattern POW_PATTERN = Pattern.compile("[0-9]\\s\\^\\s[0-9]");
+
     public int exponentiate(String expo) {
-        return 9;
+        double result = 0;
+        Matcher mat = POW_PATTERN.matcher(expo);
+        if (mat.find()) {
+            String matchedOp = mat.group();
+            result = Math.pow(Double.parseDouble(matchedOp.substring(0,1)), Double.parseDouble(matchedOp.substring(4,5)));
+        }
+        return (int)result;
     }
 }
