126cc4ce001a9df4b5890d9dcf2234aa3676e19d
Nidzad Achmedow <nidzad.achmedow@informatik.hs-fulda.de>
2020-12-31 18:50:38 +0100

    #9 added: Do not accept sinus expressions with a leading 0

--- src/test/java/MultiMath_Math/GrundrechenartenTest.java
+++ src/test/java/MultiMath_Math/GrundrechenartenTest.java
@@ -6,8 +6,14 @@ import org.junit.Assert;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.rules.ExpectedException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GrundrechenartenTest {
@@ -40,5 +46,21 @@ public class GrundrechenartenTest {
         double result = obj.calcSinus(op);
         // Assert
         assertThat(result, CoreMatchers.equalTo(expected));
+    }
+
+    @Test
+    public void sinusWithoutLeadingZero() throws IllegalArgumentException {
+        // Arrange
+        String number = "01";
+        String op = "sin(  " + number +  "      )";
+        Exception expected = assertThrows(IllegalArgumentException.class, () -> {
+            obj.calcSinus(op);
+        });
+        String expectedMsg = "No valid expression: " + op;
+        // Act
+        //double result = obj.calcSinus(op);
+        String actualMsg = expected.getMessage();
+        // Assert
+        assertTrue(actualMsg.contains(expectedMsg));
     }
 }
