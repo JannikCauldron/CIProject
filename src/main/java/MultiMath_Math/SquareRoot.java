ba414221f6dcaed1e80bbc9cf8a58d7c569cf2a2
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2021-01-01 12:02:35 +0100

    #8 added: squareroot of a one digit double

---
+++ src/main/java/MultiMath_Math/SquareRoot.java
@@ -0,0 +1,7 @@
+package MultiMath_Math;
+
+public class SquareRoot {
+    public double rooted(String root) {
+        return 3;
+    }
+}
---
+++ src/test/java/MultiMath_Math/SquareRootTest.java
@@ -0,0 +1,28 @@
+package MultiMath_Math;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SquareRootTest {
+    SquareRoot squareRoot;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        squareRoot = new SquareRoot();
+    }
+
+    @Test
+    public void rootTest() throws Exception {
+        // Arrange
+        String operation = "sqrt(9)";
+        double expected = Math.sqrt(9);
+        // Act
+        double result = squareRoot.rooted(operation);
+        // Assert
+        assertThat("wrong root", result, CoreMatchers.equalTo(expected));
+    }
+}
