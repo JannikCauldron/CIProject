848e5d9d4c03c8abbac184cdacabca96c26ea96b
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2021-01-01 12:11:49 +0100

    #8 refactor: added pattern matcher

--- src/main/java/MultiMath_Math/SquareRoot.java
+++ src/main/java/MultiMath_Math/SquareRoot.java
@@ -2,6 +2,14 @@ package MultiMath_Math;
 
 public class SquareRoot {
     public double rooted(String root) {
-        return 3;
+        String sqrtPattern = "sqrt\\([0-9]+\\)";
+        int input;
+        if (root.matches(sqrtPattern)) {
+            String numberString = root.replaceAll("[^0-9]", "");
+            input = Integer.parseInt(numberString);
+        } else {
+            return 0;
+        }
+        return Math.sqrt(input);
     }
 }
