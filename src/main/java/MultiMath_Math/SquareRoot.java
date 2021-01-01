5916ec5f7ff1ea5bb74d0d1cddad6ef170d9f13d
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2021-01-01 15:05:06 +0100

    #8 refactor: changed names, put String pattern as private

--- src/main/java/MultiMath_Math/SquareRoot.java
+++ src/main/java/MultiMath_Math/SquareRoot.java
@@ -1,15 +1,15 @@
 package MultiMath_Math;
 
 public class SquareRoot {
-    public double rooted(String root) {
-        String sqrtPattern = "sqrt\\(\\s*[0-9]+\\s*\\)";
-        int input;
-        if (root.matches(sqrtPattern)) {
-            String numberString = root.replaceAll("[^0-9]", "");
-            input = Integer.parseInt(numberString);
+    private String sqrtPattern = "sqrt\\(\\s*[0-9]+\\s*\\)";
+    public double rooted(String inputString) {
+        int inputNumber;
+        if (inputString.matches(sqrtPattern)) {
+            String numberString = inputString.replaceAll("[^0-9]", "");
+            inputNumber = Integer.parseInt(numberString);
         } else {
             return 0;
         }
-        return Math.sqrt(input);
+        return Math.sqrt(inputNumber);
     }
 }
