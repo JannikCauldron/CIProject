9f65bdf535a15abff2fb05232a755da8ea108e2b
arnofriedrich <arno-petrus.friedrich@informatik.hs-fulda.de>
2021-01-02 15:47:06 +0100

    #14 refactor: Created constant variables for better understanding

--- src/main/java/MultiMath_Math/Faculty.java
+++ src/main/java/MultiMath_Math/Faculty.java
@@ -5,6 +5,8 @@ import java.util.regex.Pattern;
 
 public class Faculty {
     private static final Pattern FAC_PATTERN = Pattern.compile("\\d+\\s*!");
+    public static final int FAC_BASE_RESULT = 1;
+    public static final int FIRST_ACTIVE_FACTOR = 2;
 
     public int facultyMatch(String operation) {
         Matcher facMatcher = FAC_PATTERN.matcher(operation);
@@ -17,11 +19,9 @@ public class Faculty {
     }
 
     public int faculty(String operation) {
-        int end = facultyMatch(operation);
-        int result = 1;
-        for (int factor = 2; factor <= end; factor++) {
-            result *= factor;
-        }
+        int input = facultyMatch(operation);
+        int result = FAC_BASE_RESULT;
+        for (int factor = FIRST_ACTIVE_FACTOR; factor <= input; factor++) result *= factor;
         return result;
     }
 }
