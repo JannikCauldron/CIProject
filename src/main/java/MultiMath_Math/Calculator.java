144fd2e180c836176ca82a01a6b262871f9d31f9
jannik.kessler <jannik.kessler@informatik.hs-fulda.de>
2021-01-04 14:13:31 +0100

    #3 refactor: improved INVALID_CHAR_PATTERN.

--- src/main/java/MultiMath_Math/Calculator.java
+++ src/main/java/MultiMath_Math/Calculator.java
@@ -7,25 +7,26 @@ public class Calculator {
 
     private static final Pattern NUMB_PATTERN = Pattern.compile("\\d+(\\.\\d+)*");
     private static final Pattern ADD_PATTERN = Pattern.compile("\\s*\\+\\s*" + NUMB_PATTERN);
-    private static final Pattern INVALID_CHAR_PATTERN = Pattern.compile("[a-zA-Z\"$§&/?#_]");
+    private static final Pattern INVALID_CHAR_PATTERN = Pattern.compile("[a-zA-ZäöüÄÖÜ\"$§&/?#_'^°|<>~{};:]");
 
     public double operate(String operation) {
         if (INVALID_CHAR_PATTERN.matcher(operation).find()) {
             throw new IllegalArgumentException("Invalid Operands for addition");
-        }
-        double operationResult = 0.0;
-        //match first integer
-        Matcher numbMatcher = NUMB_PATTERN.matcher(operation);
-        operationResult = addNumb(operationResult, numbMatcher);
-        //match additions
-        Matcher addMatcher = ADD_PATTERN.matcher(operation);
-        while (addMatcher.find()) {
-            String matchedOperation = addMatcher.group();
-            numbMatcher = NUMB_PATTERN.matcher(matchedOperation);
+        } else {
+            double operationResult = 0.0;
+            //match first integer
+            Matcher numbMatcher = NUMB_PATTERN.matcher(operation);
             operationResult = addNumb(operationResult, numbMatcher);
-        }
+            //match additions
+            Matcher addMatcher = ADD_PATTERN.matcher(operation);
+            while (addMatcher.find()) {
+                String matchedOperation = addMatcher.group();
+                numbMatcher = NUMB_PATTERN.matcher(matchedOperation);
+                operationResult = addNumb(operationResult, numbMatcher);
+            }
 
-        return operationResult;
+            return operationResult;
+        }
     }
 
     private double addNumb(double result, Matcher numbMatcher) {
