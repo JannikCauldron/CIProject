4bc6351507983abc41f6d03207b72a94e3085d89
jannik.kessler <jannik.kessler@informatik.hs-fulda.de>
2021-01-04 15:18:21 +0100

    #4 refactor: added two patterns and improved operate().

--- src/main/java/MultiMath_Math/Subtraction.java
+++ src/main/java/MultiMath_Math/Subtraction.java
@@ -1,7 +1,27 @@
 package MultiMath_Math;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Subtraction {
+    public Pattern subtractionPattern = Pattern.compile("\\d+ - \\d+");
+    public Pattern numbPattern = Pattern.compile("\\d+");
+
     public int operate(String operation) {
-        return 9;
+        int result = 0;
+
+        Matcher subMatcher = subtractionPattern.matcher(operation);
+        if (subMatcher.find()) {
+            String match = subMatcher.group();
+            Matcher numbMatcher = numbPattern.matcher(match);
+            if (numbMatcher.find()) {
+                result = Integer.parseInt(numbMatcher.group(0));
+            }
+            if (numbMatcher.find()) {
+                result -=  Integer.parseInt(numbMatcher.group(0));
+            }
+        }
+
+        return result;
     }
 }
