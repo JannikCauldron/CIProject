c0342186f925162c5490e02a9fe31a5681461a5e
jannik.kessler <jannik.kessler@informatik.hs-fulda.de>
2021-01-04 15:49:16 +0100

    #4 refactor: extract method subNumb. changed some names.

--- src/main/java/MultiMath_Math/Subtraction.java
+++ src/main/java/MultiMath_Math/Subtraction.java
@@ -8,22 +8,27 @@ public class Subtraction {
     public Pattern numbPattern = Pattern.compile("\\d+");
 
     public int operate(String operation) {
-        int result = 0;
-
+        int operationResult = 0;
+        //first number to result
         Matcher numbMatcher = numbPattern.matcher(operation);
         if (numbMatcher.find()) {
-            result = Integer.parseInt(numbMatcher.group());
+            operationResult = Integer.parseInt(numbMatcher.group());
         }
-
+        //subtract every following number
         Matcher subMatcher = subtractionPattern.matcher(operation);
         while (subMatcher.find()) {
-            String match = subMatcher.group();
-            numbMatcher = numbPattern.matcher(match);
-            if (numbMatcher.find()) {
-                result -= Integer.parseInt(numbMatcher.group(0));
-            }
+            String nextSubMatch = subMatcher.group();
+            numbMatcher = numbPattern.matcher(nextSubMatch);
+            operationResult = subNumb(operationResult, numbMatcher);
         }
 
+        return operationResult;
+    }
+
+    private int subNumb(int result, Matcher numbMatcher) {
+        if (numbMatcher.find()) {
+            result -= Integer.parseInt(numbMatcher.group());
+        }
         return result;
     }
 }
