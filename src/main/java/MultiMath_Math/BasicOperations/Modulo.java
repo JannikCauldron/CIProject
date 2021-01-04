c05de7a90333ca6a57d94b0218c8e5c031d91f63
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2021-01-04 17:28:55 +0100

    #12 refactor: changed variable names, changed pattern to final

--- src/main/java/MultiMath_Math/BasicOperations/Modulo.java
+++ src/main/java/MultiMath_Math/BasicOperations/Modulo.java
@@ -5,15 +5,15 @@ import java.util.regex.Matcher;
 
 public class Modulo {
 
-    private Pattern MOD_PATTERN = Pattern.compile("[0-9]\\s*%\\s*[0-9]");
+    private final Pattern MOD_PATTERN = Pattern.compile("[0-9]\\s*%\\s*[0-9]");
 
     public double moduloOp(String operation) {
         double result = 0;
-        Matcher mat = MOD_PATTERN.matcher(operation);
-        if (mat.find()) {
-            String matchedOp = mat.group();
-            int matchedOpLength = matchedOp.length();
-            result = Double.parseDouble(matchedOp.substring(0,1)) % Double.parseDouble(matchedOp.substring(matchedOpLength-1,matchedOpLength));
+        Matcher modMatcher = MOD_PATTERN.matcher(operation);
+        if (modMatcher.find()) {
+            String matchedOperation = modMatcher.group();
+            int matchedOperationLength = matchedOperation.length();
+            result = Double.parseDouble(matchedOperation.substring(0,1)) % Double.parseDouble(matchedOperation.substring(matchedOperationLength-1,matchedOperationLength));
         }
         return result;
     }
