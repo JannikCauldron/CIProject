32f5256c37371d347921b058f462e96eac0d6875
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2021-01-04 16:56:01 +0100

    #12 refactor: pattern matcher added

--- src/main/java/MultiMath_Math/BasicOperations/Modulo.java
+++ src/main/java/MultiMath_Math/BasicOperations/Modulo.java
@@ -1,8 +1,19 @@
 package MultiMath_Math.BasicOperations;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Modulo {
 
+    private Pattern MOD_PATTERN = Pattern.compile("[0-9]\\s%\\s[0-9]");
+
     public double moduloOp(String operation) {
-        return 1;
+        double result = 0;
+        Matcher mat = MOD_PATTERN.matcher(operation);
+        if (mat.find()) {
+            String matchedOp = mat.group();
+            result = Double.parseDouble(matchedOp.substring(0,1)) % Double.parseDouble(matchedOp.substring(4,5));
+        }
+        return result;
     }
 }
