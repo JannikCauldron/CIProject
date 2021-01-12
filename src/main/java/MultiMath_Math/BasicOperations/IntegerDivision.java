93e652f31356affe4c46a31746eff9f8935c711b
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2021-01-12 18:00:21 +0100

    #13 refactor: Pattern matcher added instead of simple return statement

--- src/main/java/MultiMath_Math/BasicOperations/IntegerDivision.java
+++ src/main/java/MultiMath_Math/BasicOperations/IntegerDivision.java
@@ -1,7 +1,19 @@
 package MultiMath_Math.BasicOperations;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class IntegerDivision {
+
+    private Pattern DIV_PATTERN = Pattern.compile("div\\([0-9],[0-9]\\)");
+
     public int div(String operation) {
-        return 2;
+        int result = 0;
+        Matcher divMatcher = DIV_PATTERN.matcher(operation);
+        if (divMatcher.find()) {
+            String matchedOperation = divMatcher.group();
+            result = Integer.parseInt(matchedOperation.substring(4,5)) / Integer.parseInt(matchedOperation.substring(6,7));
+        }
+        return result;
     }
 }
