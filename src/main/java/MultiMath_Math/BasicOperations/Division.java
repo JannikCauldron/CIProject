85a1f51fd91da75fc7a09fefe958b15826b10ffc
Christian Alexander Hammer <christian-alexander.hammer@informatik.hs-fulda.de>
2021-01-12 18:30:58 +0100

    #6 refactored: Added parameter for getRoundedValue to reduce repeating code, reduced unecessary imports

--- src/main/java/MultiMath_Math/BasicOperations/Division.java
+++ src/main/java/MultiMath_Math/BasicOperations/Division.java
@@ -4,11 +4,12 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
-import static java.lang.Math.abs;
 
-import static java.lang.Math.abs;
 
 public class Division {
+
+    private static String DOUBLE_DECIMALFORMAT = "#.####";
+    private static String INT_DECIMALFORMAT = "#";
 
     public static double divide(double[] divValues) {
         double quotient = divValues[0];
@@ -17,7 +18,7 @@ public class Division {
         for (int i = 1; i < divValues.length; i++) {
             quotient /= divValues[i];
         }
-        productString = getRoundedValue(quotient);
+        productString = getRoundedValue(quotient, DOUBLE_DECIMALFORMAT);
         return Double.parseDouble(productString);
     }
 
@@ -28,32 +29,17 @@ public class Division {
         for (int i = 1; i < intValues.length; i++) {
             quotient /= (double)intValues[i];
         }
-        productString = getRoundedIntValue(quotient);
+        productString = getRoundedValue(quotient, INT_DECIMALFORMAT);
         return Integer.parseInt(productString);
     }
 
-    private static String getRoundedValue(double product) {
+    private static String getRoundedValue(double product, String decimalFormat) {
         String productString;
-        DecimalFormat df = new DecimalFormat("#.####");
+        DecimalFormat df = new DecimalFormat(decimalFormat);
         df.setDecimalFormatSymbols(DecimalFormatSymbols.getInstance(Locale.US));
         df.setRoundingMode(RoundingMode.HALF_UP);
         productString = df.format(product);
         return productString;
     }
-
-
-    private static String getRoundedIntValue(double product) {
-        String productString;
-        DecimalFormat df = new DecimalFormat("#");
-        df.setDecimalFormatSymbols(DecimalFormatSymbols.getInstance(Locale.US));
-        df.setRoundingMode(RoundingMode.HALF_UP);
-        productString = df.format(product);
-        return productString;
-    }
-
-
-
-
-
 
 }
