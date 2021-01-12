61b9bcead0e3d3b54e6632929d017ed4cde246f0
Christian Alexander Hammer <christian-alexander.hammer@informatik.hs-fulda.de>
2021-01-12 18:25:32 +0100

    #6 added: Test and implementation of division with integer

--- src/main/java/MultiMath_Math/BasicOperations/Division.java
+++ src/main/java/MultiMath_Math/BasicOperations/Division.java
@@ -4,6 +4,9 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
+import static java.lang.Math.abs;
+
+import static java.lang.Math.abs;
 
 public class Division {
 
@@ -18,6 +21,16 @@ public class Division {
         return Double.parseDouble(productString);
     }
 
+    public static int divide(int[] intValues) {
+        double quotient = (double)intValues[0];
+        String productString;
+
+        for (int i = 1; i < intValues.length; i++) {
+            quotient /= (double)intValues[i];
+        }
+        productString = getRoundedIntValue(quotient);
+        return Integer.parseInt(productString);
+    }
 
     private static String getRoundedValue(double product) {
         String productString;
@@ -27,4 +40,20 @@ public class Division {
         productString = df.format(product);
         return productString;
     }
+
+
+    private static String getRoundedIntValue(double product) {
+        String productString;
+        DecimalFormat df = new DecimalFormat("#");
+        df.setDecimalFormatSymbols(DecimalFormatSymbols.getInstance(Locale.US));
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        productString = df.format(product);
+        return productString;
+    }
+
+
+
+
+
+
 }
--- src/test/java/MultiMath_Math/BasicOperations/DivisionTest.java
+++ src/test/java/MultiMath_Math/BasicOperations/DivisionTest.java
@@ -4,6 +4,7 @@ import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 
+
 public class DivisionTest {
 
 
@@ -11,10 +12,10 @@ public class DivisionTest {
     public void doubleMultiplicationTest() throws Exception {
         // Arrange
         double[][] divArray = {{5d, 2d},         // with decimals
-                {50d, 2d, 5d, 5d},    // without decimals
-                {0.05, 1000},                 // division below 4 decimals and round half_up
-                {7531, 73},                       // ###.####
-                {-0.001d, 0.03d}};                    // periodic number 0.033333333
+                {50d, 2d, 5d, 5d},               // without decimals
+                {0.05, 1000},                    // division below 4 decimals and round half_up
+                {7531, 73},                      // ###.####
+                {-0.001d, 0.03d}};               // periodic number 0.033333333
         double[] expectedQuotientArray = {2.5d, 1d, 0.0001d, 103.1644d, -0.0333d};
         // Act
         int ArrayLength = divArray.length;
@@ -25,4 +26,26 @@ public class DivisionTest {
         // Assert
         MatcherAssert.assertThat("Double Division Test:", resultArray, CoreMatchers.equalTo(expectedQuotientArray));
     }
+
+    @Test
+    public void integerDivisionTest() throws Exception {
+        // Arrange
+        // Arrange
+        int[][] divArray = {{5, 2},         // round up
+                {50, 2, 5, 5},                 // no rounding necessary
+                {12, 11},                      // round down
+                {0, 1000},                     // zero dividing
+                {-1, -1, -1, -1, -1}};         // negative numbers
+        int[] expectedQuotientArray = {3, 1, 1, 0, -1};
+        // Act
+        int ArrayLength = divArray.length;
+        int[] resultArray = new int[ArrayLength];
+        for (int i = 0; i < ArrayLength; i++) {
+            resultArray[i] = Division.divide(divArray[i]);
+        }
+        // Assert
+        MatcherAssert.assertThat("Integer Division Test:", resultArray, CoreMatchers.equalTo(expectedQuotientArray));
+    }
+
+
 }
