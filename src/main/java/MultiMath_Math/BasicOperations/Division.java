f8876b319d0ab88d8211477fd0de9120ea15df54
Christian Alexander Hammer <christian-alexander.hammer@informatik.hs-fulda.de>
2021-01-12 21:11:58 +0100

    #6 refactored: Simplyfied method ifZero. Fixed typo in final variables. Suppressed Warnings for redundant warnings on local variables.

--- src/main/java/MultiMath_Math/BasicOperations/Division.java
+++ src/main/java/MultiMath_Math/BasicOperations/Division.java
@@ -10,9 +10,10 @@ import java.util.Locale;
 
 public class Division {
 
-    private static String DOUBLE_DECIMALFORMAT = "#.####";
-    private static String INT_DECIMALFORMAT = "#";
+    private static final String DOUBLE_DECIMAL_FORMAT = "#.####";
+    private static final String INT_DECIMAL_FORMAT = "#";
 
+    @SuppressWarnings("UnnecessaryLocalVariable")
     public static double divide(double... divValues) throws DividedByZeroException {
         double quotient = divValues[0];
         String productString;
@@ -23,22 +24,22 @@ public class Division {
             }
             quotient /= divValues[i];
         }
-        productString = getRoundedValue(quotient, DOUBLE_DECIMALFORMAT);
+        productString = getRoundedValue(quotient, DOUBLE_DECIMAL_FORMAT);
         return Double.parseDouble(productString);
     }
 
-
+    @SuppressWarnings("UnnecessaryLocalVariable")
     public static int divide(int... intValues) throws DividedByZeroException {
-        double quotient = (double) intValues[0];
+        double quotient = intValues[0];
         String productString;
 
         for (int i = 1; i < intValues.length; i++) {
-            if (isZero((double) intValues[i])) {
+            if (isZero(intValues[i])) {
                 throw new DividedByZeroException("You can't divide by 0! Divisor: 0");
             }
-            quotient /= (double) intValues[i];
+            quotient /= intValues[i];
         }
-        productString = getRoundedValue(quotient, INT_DECIMALFORMAT);
+        productString = getRoundedValue(quotient, INT_DECIMAL_FORMAT);
         return Integer.parseInt(productString);
     }
 
@@ -53,10 +54,7 @@ public class Division {
     }
 
     private static boolean isZero(double number) {
-        if (number == 0.0d) {
-            return true;
-        }
-        return false;
+        return number == 0.0d;
     }
 
 }
