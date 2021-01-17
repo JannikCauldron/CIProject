8671d7a874dcdad130320c6f7c8aa6e3bc28fd3a
Christian Alexander Hammer <christian-alexander.hammer@informatik.hs-fulda.de>
2021-01-17 19:23:32 +0100

    #19 refactor: Reformatted Code

--- src/test/java/MultiMath_Math/BasicOperations/NumerologyTest.java
+++ src/test/java/MultiMath_Math/BasicOperations/NumerologyTest.java
@@ -1,22 +1,23 @@
 package MultiMath_Math.BasicOperations;
 
-import org.junit.jupiter.api.Test;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
 
 public class NumerologyTest {
 
     @Test
     public void isEvenTest() {
         // Arrange
-        int[] operations = {0,1,2,3,11,12,999998};
+        int[] operations = {0, 1, 2, 3, 11, 12, 999998};
         boolean[] expected = {true, false, true, false, false, true, true};
         boolean[] results = new boolean[expected.length];
         // Act
-        for(int i = 0; i< expected.length; i++) {
+        for (int i = 0; i < expected.length; i++) {
             results[i] = Numerology.isEven(operations[i]);
         }
         // Assert
         MatcherAssert.assertThat("Is Even Test", results, CoreMatchers.equalTo(expected));
     }
+
 }
