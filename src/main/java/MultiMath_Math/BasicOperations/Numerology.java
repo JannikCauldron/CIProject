48afa0478c9d5da296e5f8b6c9ce13b91abf4e7b
Christian Alexander Hammer <christian-alexander.hammer@informatik.hs-fulda.de>
2021-01-17 19:36:29 +0100

    #19 added: Test and Implementation of is Prime

--- src/main/java/MultiMath_Math/BasicOperations/Numerology.java
+++ src/main/java/MultiMath_Math/BasicOperations/Numerology.java
@@ -1,5 +1,7 @@
 package MultiMath_Math.BasicOperations;
 
+import java.math.BigInteger;
+
 public class Numerology {
 
     public static boolean isEven(int number) {
@@ -8,5 +10,10 @@ public class Numerology {
 
     public static boolean isOdd(int number) {
         return number % 2 != 0;
+    }
+
+    public static boolean isPrime(int number) {
+        BigInteger bigInt = BigInteger.valueOf(number);
+        return bigInt.isProbablePrime(100);
     }
 }
