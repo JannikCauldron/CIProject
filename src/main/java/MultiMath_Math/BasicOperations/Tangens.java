ee10512cd7124e23a05e9b98e0e0c897a8b0ef54
Nidzad Achmedow <nidzad.achmedow@informatik.hs-fulda.de>
2021-01-24 17:16:26 +0100

    #11 refactor: Add pattern to match tan expression and return result after processing expression

--- src/main/java/MultiMath_Math/BasicOperations/Tangens.java
+++ src/main/java/MultiMath_Math/BasicOperations/Tangens.java
@@ -3,6 +3,14 @@ package MultiMath_Math.BasicOperations;
 public class Tangens {
 
     public double calcTangens(String input) {
-        return Math.tan(5);
+        int result;
+        String pattern = "^tan\\(\\d\\)$";
+
+        if (input.matches(pattern)) {
+            String extractNumber = input.replaceAll("\\D", "");
+            result = Integer.parseInt(extractNumber);
+            return Math.tan(result);
+        }
+        return -1;
     }
 }
