083327d091f8a6bd0f13e33f1a476d3bf13cf5df
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2021-01-26 16:48:03 +0100

    #15 refactor: changed names, put String pattern as private

--- src/main/java/MultiMath_Math/BasicOperations/Logarithm.java
+++ src/main/java/MultiMath_Math/BasicOperations/Logarithm.java
@@ -1,15 +1,16 @@
 package MultiMath_Math.BasicOperations;
 
 public class Logarithm {
-    public double log(String op) {
-        String logPattern = "log\\s*\\(\\s*[0-9]+\\s*\\)";
-        double input;
-        if (op.matches(logPattern)) {
-            String numberString = op.replaceAll("[^0-9]", "");
-            input = Double.parseDouble(numberString);
+    private String logPattern = "log\\s*\\(\\s*[0-9]+\\s*\\)";
+
+    public double log(String inputString) {
+        double inputNumber;
+        if (inputString.matches(logPattern)) {
+            String numberString = inputString.replaceAll("[^0-9]", "");
+            inputNumber = Double.parseDouble(numberString);
         } else {
             return 0;
         }
-        return Math.log(input);
+        return Math.log(inputNumber);
     }
 }
