12cd9a4506d361ddf37bf01bc58d63a6a6d1dd34
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2021-01-26 16:39:03 +0100

    #15 refactor: replaced simple return to pattern matcher

--- src/main/java/MultiMath_Math/BasicOperations/Logarithm.java
+++ src/main/java/MultiMath_Math/BasicOperations/Logarithm.java
@@ -2,6 +2,14 @@ package MultiMath_Math.BasicOperations;
 
 public class Logarithm {
     public double log(String op) {
-        return 1.6094379124341003;
+        String logPattern = "log\\([0-9]+\\)";
+        double input;
+        if (op.matches(logPattern)) {
+            String numberString = op.replaceAll("[^0-9]", "");
+            input = Double.parseDouble(numberString);
+        } else {
+            return 0;
+        }
+        return Math.log(input);
     }
 }
