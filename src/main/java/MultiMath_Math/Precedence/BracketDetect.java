090aa48733c16d85f92732a68ada831c051d91f4
jannik.kessler <jannik.kessler@informatik.hs-fulda.de>
2021-01-26 12:11:33 +0100

    #16 added: twoBracketsDetection test. changed return type of detect() to String[]. added Stack algorithm to implement detection of two brackets.

--- src/main/java/MultiMath_Math/Precedence/BracketDetect.java
+++ src/main/java/MultiMath_Math/Precedence/BracketDetect.java
@@ -1,20 +1,38 @@
 package MultiMath_Math.Precedence;
 
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BracketDetect {
     private Pattern bracketPattern = Pattern.compile("\\(.+\\)");
 
-    public String detect(String operation) {
+    public String[] detect(String operation) {
         String result = "";
 
         Matcher bracketMatcher = bracketPattern.matcher(operation);
-        if (bracketMatcher.find()) {
+        Stack opStack = new Stack();
+        int cnt = 0;
+        while (bracketMatcher.find()) {
+            cnt++;
             String match = bracketMatcher.group();
             result = match.substring(1, match.length() - 1);
+
+            Matcher secondBracketMatcher = bracketPattern.matcher(result);
+            if (secondBracketMatcher.find()) {
+                cnt++;
+                String secondMatch = secondBracketMatcher.group();
+                opStack.push(result.replace(secondMatch, ""));
+                opStack.push(secondMatch.substring(1, secondMatch.length() - 1));
+            } else {
+                opStack.push(result);
+            }
+        }
+        String resultOperation[] = new String[cnt];
+        for (int i = 0; i < cnt; i++) {
+            resultOperation[i] = (String) opStack.pop();
         }
 
-        return result;
+        return resultOperation;
     }
 }
--- src/test/java/MultiMath_Math/Precedence/BracketDetectTest.java
+++ src/test/java/MultiMath_Math/Precedence/BracketDetectTest.java
@@ -20,7 +20,18 @@ public class BracketDetectTest {
         String operation = "(2 + 5)";
         String expectedResult = "2 + 5";
         //act
-        String actualResult = detector.detect(operation);
+        String actualResult = detector.detect(operation)[0];
+        //assert
+        assertThat("Erkennung von " + operation + " wurde fehlerhaft erkannt als " + actualResult, actualResult, CoreMatchers.equalTo(expectedResult));
+    }
+
+    @Test
+    void twoBracketsDetection() {
+        //arrange
+        String operation = "((2 + 5) * 8)";
+        String[] expectedResult = new String[] {"2 + 5", " * 8"};
+        //act
+        String[] actualResult = detector.detect(operation);
         //assert
         assertThat("Erkennung von " + operation + " wurde fehlerhaft erkannt als " + actualResult, actualResult, CoreMatchers.equalTo(expectedResult));
     }
