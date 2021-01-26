f7c3a554e389ddcff53d2ff8bf0e901ba071a97e
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2021-01-26 16:35:03 +0100

    #15 added: natural logarithm of a one digit number

---
+++ src/main/java/MultiMath_Math/BasicOperations/Logarithm.java
@@ -0,0 +1,7 @@
+package MultiMath_Math.BasicOperations;
+
+public class Logarithm {
+    public double log(String op) {
+        return 1.6094379124341003;
+    }
+}
---
+++ src/test/java/MultiMath_Math/BasicOperations/LogarithmTest.java
@@ -0,0 +1,26 @@
+package MultiMath_Math.BasicOperations;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertThat;
+
+public class LogarithmTest {
+    Logarithm logarithm;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        logarithm = new Logarithm();
+    }
+
+    @Test
+    public void logTest() throws Exception {
+        //Arrange
+        String operation = "log(5)";
+        double expected = Math.log(5);
+        //Act
+        double result = logarithm.log(operation);
+        //Assert
+        assertThat("wrong logaritm", result, CoreMatchers.equalTo(expected));
+    }
+}
