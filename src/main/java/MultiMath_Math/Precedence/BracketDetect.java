d07c6db316d9c2b3fe5c31f1609558bb31db2c3c
jannik.kessler <jannik.kessler@informatik.hs-fulda.de>
2021-01-26 11:23:24 +0100

    #16 refactor: added bracketPattern and simple matching algorithm to extract the content of a bracket.

--- src/main/java/MultiMath_Math/Precedence/BracketDetect.java
+++ src/main/java/MultiMath_Math/Precedence/BracketDetect.java
@@ -1,7 +1,20 @@
 package MultiMath_Math.Precedence;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class BracketDetect {
+    private Pattern bracketPattern = Pattern.compile("\\(.+\\)");
+
     public String detect(String operation) {
-        return "2 + 5";
+        String result = "";
+
+        Matcher bracketMatcher = bracketPattern.matcher(operation);
+        if (bracketMatcher.find()) {
+            String match = bracketMatcher.group();
+            result = match.substring(1, match.length() - 1);
+        }
+
+        return result;
     }
 }
