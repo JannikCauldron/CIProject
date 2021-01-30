4179ba7e7488c005d4f55e2eb887c83863e2f16b
Lucy Jochum <lucy.jochum@informatik.hs-fulda.de>
2021-01-30 17:21:39 +0100

    #15 refactor: changed decimal extraction to call to method from DecimalFilter, deleted unused variables

--- src/main/java/MultiMath_Math/BasicOperations/NaturalLogarithm.java
+++ src/main/java/MultiMath_Math/BasicOperations/NaturalLogarithm.java
@@ -6,27 +6,10 @@ public class NaturalLogarithm {
 
     public double logn(String operation) {
         double number;
-        int indexOfDot;
-        String stringBeforeDot;
-        String stringAfterDot;
         int operationStringLength = operation.length();
 
         if (operation.matches(LOGN_PATTERN)) {
-            if (operation.contains(".")) {
-                indexOfDot = operation.indexOf('.');
-
-                stringBeforeDot = operation.substring(0, indexOfDot);
-                stringAfterDot = operation.substring(indexOfDot+1, operationStringLength);
-
-                String numberStringBeforeDot = stringBeforeDot.replaceAll("[^0-9]", "");
-                String numberStringAfterDot = stringAfterDot.replaceAll("[^0-9]", "");
-
-                String completeNumberString = numberStringBeforeDot + "." + numberStringAfterDot;
-                number = Double.parseDouble(completeNumberString);
-            } else {
-                String completeNumberString = operation.replaceAll("[^0-9]", "");
-                number = Double.parseDouble(completeNumberString);
-            }
+            number = DecimalFilter.getNumber(operation, operationStringLength);
         } else {
             throw new IllegalArgumentException("no valid natural logarithm expression: " + operation);
         }
