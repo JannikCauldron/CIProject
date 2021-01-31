7e22780307ccad94ddd86deb1877867b80b1a886
arnofriedrich <arno-petrus.friedrich@informatik.hs-fulda.de>
2021-01-31 20:02:23 +0100

    #21 refactor: New constants + better names

--- src/main/java/MultiMath_Math/NumeralSystems/DecimalToHexadecimal.java
+++ src/main/java/MultiMath_Math/NumeralSystems/DecimalToHexadecimal.java
@@ -3,15 +3,17 @@ package MultiMath_Math.NumeralSystems;
 public class DecimalToHexadecimal {
 
     private static final char[] HEXCHARS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    private static final int HEX_BASE = 16;
+    public static final String EMPTY_STRING = "";
 
     public String convert(int decimal) {
-        String result = "";
-        int tmp;
+        String hex = EMPTY_STRING;
+        int hextmp;
         while(decimal > 0) {
-            tmp = decimal % 16;
-            result = HEXCHARS[tmp] + result;
-            decimal /= 16;
+            hextmp = decimal % HEX_BASE;
+            hex = HEXCHARS[hextmp] + hex;
+            decimal /= HEX_BASE;
         }
-        return result;
+        return hex;
     }
 }
