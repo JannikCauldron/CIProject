6ec433e8bbadbbee9d56cc88c8785c20577fa1bd
arnofriedrich <arno-petrus.friedrich@informatik.hs-fulda.de>
2021-01-31 20:16:46 +0100

    #21 refactor: Use of StringBuilder instead of concat in loop

--- src/main/java/MultiMath_Math/NumeralSystems/DecimalToHexadecimal.java
+++ src/main/java/MultiMath_Math/NumeralSystems/DecimalToHexadecimal.java
@@ -2,18 +2,20 @@ package MultiMath_Math.NumeralSystems;
 
 public class DecimalToHexadecimal {
 
-    private static final char[] HEXCHARS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    private static final char[] HEXCHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     private static final int HEX_BASE = 16;
-    public static final String EMPTY_STRING = "";
+    private static final String EMPTY_STRING = "";
+    private static final int START_INDEX = 0;
+    private static StringBuilder hex;
 
     public String convert(int decimal) {
-        String hex = EMPTY_STRING;
-        int hextmp;
+        hex = new StringBuilder(EMPTY_STRING);
+        int hexTmp;
         while(decimal > 0) {
-            hextmp = decimal % HEX_BASE;
-            hex = HEXCHARS[hextmp] + hex;
+            hexTmp = decimal % HEX_BASE;
+            hex.insert(START_INDEX, HEXCHARS[hexTmp]);
             decimal /= HEX_BASE;
         }
-        return hex;
+        return hex.toString();
     }
 }
