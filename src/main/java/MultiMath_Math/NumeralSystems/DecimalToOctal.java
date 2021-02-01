a955bcc2f50f429b69302b617a42b9c2c47371db
arnofriedrich <arno-petrus.friedrich@informatik.hs-fulda.de>
2021-02-01 16:06:33 +0100

    #22 refactor: New names + StringBuilder + constants

--- src/main/java/MultiMath_Math/NumeralSystems/DecimalToOctal.java
+++ src/main/java/MultiMath_Math/NumeralSystems/DecimalToOctal.java
@@ -3,17 +3,18 @@ package MultiMath_Math.NumeralSystems;
 public class DecimalToOctal {
 
     private static final char[] OCT_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7'};
-    private static final String EMPTY_STRING = "";
     private static final int OCT_BASE = 8;
+    public static final int START_INDEX = 0;
+    private StringBuilder oct;
 
     public String convert(int decimal) {
-        String result = EMPTY_STRING;
-        int tmp;
+        oct = new StringBuilder();
+        int rest;
         while(decimal > 0) {
-            tmp = decimal % OCT_BASE;
-            result = OCT_CHARS[tmp] + result;
+            rest = decimal % OCT_BASE;
+            oct.insert(START_INDEX, OCT_CHARS[rest]);
             decimal /= OCT_BASE;
         }
-        return result;
+        return oct.toString();
     }
 }
