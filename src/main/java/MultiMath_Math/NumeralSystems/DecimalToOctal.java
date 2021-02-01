5f6e231bfb77cfaf3a8ba063175f0bcfe246b4d4
arnofriedrich <arno-petrus.friedrich@informatik.hs-fulda.de>
2021-02-01 16:57:25 +0100

    #22 refactor: Encapsulation of class variables

--- src/main/java/MultiMath_Math/NumeralSystems/DecimalToOctal.java
+++ src/main/java/MultiMath_Math/NumeralSystems/DecimalToOctal.java
@@ -4,7 +4,7 @@ public class DecimalToOctal {
 
     private static final char[] OCT_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7'};
     private static final int OCT_BASE = 8;
-    public static final int START_INDEX = 0;
+    private static final int START_INDEX = 0;
     private StringBuilder oct;
 
     public String convert(int decimal) throws Exception {
--- src/test/java/MultiMath_Math/NumeralSystems/DecimalToOctalTest.java
+++ src/test/java/MultiMath_Math/NumeralSystems/DecimalToOctalTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DecimalToOctalTest {
-    DecimalToOctal converter;
+    private DecimalToOctal converter;
 
     @BeforeEach
     void setUp() {
