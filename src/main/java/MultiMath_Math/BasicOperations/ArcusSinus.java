04300f4ce9c12f7565e7c69d189da38bc0ff5c2a
Nidzad Achmedow <nidzad.achmedow@informatik.hs-fulda.de>
2021-02-02 17:29:21 +0100

    #38 refactor: Introduce constants and methods to make it more readable

--- src/main/java/MultiMath_Math/BasicOperations/ArcusSinus.java
+++ src/main/java/MultiMath_Math/BasicOperations/ArcusSinus.java
@@ -2,8 +2,26 @@ package MultiMath_Math.BasicOperations;
 
 public class ArcusSinus {
 
-	public double calcArcSin(String operation) {
-		return Math.asin(5);
+	private static final String PATTERN_TO_REPLACE = "\\D";
+    private static final String ARCSIN_PATTERN = "arcsin\\(\\d\\)";
+
+    public double calcArcSin(String expression) {
+        String arcSinPattern = ARCSIN_PATTERN;
+
+        if (matchArcSinExpression(expression, arcSinPattern)) {
+            return Math.asin(extractNumberFromArcSinString(expression));
+        }
+        
+        return -1;
 	}
+
+    private boolean matchArcSinExpression(String expression, String arcSinPattern) {
+        return expression.matches(arcSinPattern);
+    }
+
+    private double extractNumberFromArcSinString(String operation) {
+        String extractedNumberString = operation.replaceAll(PATTERN_TO_REPLACE, "");
+        return Double.parseDouble(extractedNumberString);
+    }
 
 }
