3d23ce0530d24568b1a031b4dbff88047be9e54b
Nidzad Achmedow <nidzad.achmedow@informatik.hs-fulda.de>
2021-02-03 19:26:26 +0100

    #38 refactor: Rename parameters of methods to be more specific about their role

--- src/main/java/MultiMath_Math/BasicOperations/ArcusSinus.java
+++ src/main/java/MultiMath_Math/BasicOperations/ArcusSinus.java
@@ -2,29 +2,29 @@ package MultiMath_Math.BasicOperations;
 
 public class ArcusSinus {
 
-	private static final int ERROR = -1;
+    private static final int ERROR = -1;
     private static final String PATTERN_TO_REPLACE = "\\D";
     private static final String ARCSIN_PATTERN = "arcsin\\(\\s*-?\\d+\\s*\\)";
 
-    public double processArcSinExpression(String expression) {
+    public double processArcSinExpression(String arcSinExpression) {
 
-        if (matchArcSinExpression(expression)) {
-            return calcArcusSinus(expression);
+        if (matchArcSinExpression(arcSinExpression)) {
+            return calcArcusSinus(arcSinExpression);
         }
-        
+
         return ERROR;
-	}
-
-    private double calcArcusSinus(String expression) {
-        return Math.asin(extractNumberFromArcSinString(expression));
     }
 
-    private boolean matchArcSinExpression(String expression) {
-        return expression.matches(ARCSIN_PATTERN);
+    private double calcArcusSinus(String arcSinValue) {
+        return Math.asin(extractNumberFromArcSinString(arcSinValue));
     }
 
-    private double extractNumberFromArcSinString(String operation) {
-        String extractedNumberString = operation.replaceAll(PATTERN_TO_REPLACE, "");
+    private boolean matchArcSinExpression(String expressionToMatch) {
+        return expressionToMatch.matches(ARCSIN_PATTERN);
+    }
+
+    private double extractNumberFromArcSinString(String stringExpression) {
+        String extractedNumberString = stringExpression.replaceAll(PATTERN_TO_REPLACE, "");
         return Double.parseDouble(extractedNumberString);
     }
 
