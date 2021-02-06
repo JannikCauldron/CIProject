7bb6ac208c0175ce76f6c80b8d2eb7c9765bd93a
arnofriedrich <arno-petrus.friedrich@informatik.hs-fulda.de>
2021-02-06 22:42:04 +0100

    #41 refactor: New constant + extracted loop in new method

--- src/main/java/MultiMath_Math/NumeralSystems/HexadecimalToDecimal.java
+++ src/main/java/MultiMath_Math/NumeralSystems/HexadecimalToDecimal.java
@@ -3,13 +3,18 @@ package MultiMath_Math.NumeralSystems;
 public class HexadecimalToDecimal {
 
     private static final String HEX_CHARS = "0123456789abcdef";
+    private static final int HEX_BASE = 16;
 
     public int convert(String hex) {
         hex = hex.toLowerCase();
-        int result = 0;
-        for(int i = 0; i < hex.length(); i++) {
-            result = 16 * result + HEX_CHARS.indexOf(hex.charAt(i));
+        return calculateDec_CharByChar(hex);
+    }
+
+    private int calculateDec_CharByChar(String hex) {
+        int decimal = 0;
+        for(int index = 0; index < hex.length(); index++) {
+            decimal = HEX_BASE * decimal + HEX_CHARS.indexOf(hex.charAt(index));
         }
-        return result;
+        return decimal;
     }
 }
