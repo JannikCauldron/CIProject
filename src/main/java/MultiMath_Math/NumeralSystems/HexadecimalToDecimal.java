8fb0f4d6e52171031e53d7aaef91d36c4cea8e3e
arnofriedrich <arno-petrus.friedrich@informatik.hs-fulda.de>
2021-02-06 16:15:12 +0100

    #41 refactor: Added constant

--- src/main/java/MultiMath_Math/NumeralSystems/HexadecimalToDecimal.java
+++ src/main/java/MultiMath_Math/NumeralSystems/HexadecimalToDecimal.java
@@ -1,8 +1,10 @@
 package MultiMath_Math.NumeralSystems;
 
 public class HexadecimalToDecimal {
+
+    private static final String HEX_CHARS = "0123456789abcdef";
+
     public int convert(String hex) {
-        String hexChars = "0123456789abcdef";
-        return hexChars.indexOf(hex);
+        return HEX_CHARS.indexOf(hex);
     }
 }
