52824cfb259a47b0ae7f608f2cb520312e67bdb8
arnofriedrich <arno-petrus.friedrich@informatik.hs-fulda.de>
2021-02-08 13:57:16 +0100

    #22 refactor: Introduced constants

--- src/main/java/MultiMath_Math/NumeralSystems/DecimalToOctal.java
+++ src/main/java/MultiMath_Math/NumeralSystems/DecimalToOctal.java
@@ -5,12 +5,14 @@ public class DecimalToOctal {
     private static final char[] OCT_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7'};
     private static final int OCT_BASE = 8;
     private static final int START_INDEX = 0;
+    private static final String NEGATIVE_VALUE_EXCEPTION_MESSAGE = "No conversion of negative values!";
+    private static final String RESULT_FOR_ZERO = "0";
     private StringBuilder oct;
 
     public String convert(int decimal) throws Exception {
 
-        if (decimal == 0) return "0";
-        if (decimal < 0) throw new Exception("No conversion of negative values!");
+        if (decimal == 0) return RESULT_FOR_ZERO;
+        if (decimal < 0) throw new Exception(NEGATIVE_VALUE_EXCEPTION_MESSAGE);
 
         oct = new StringBuilder();
         int rest;
