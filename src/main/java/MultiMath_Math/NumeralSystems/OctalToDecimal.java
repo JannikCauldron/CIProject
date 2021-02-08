7bff59904fc97411b1a9a06016277c5d1eb43bcd
arnofriedrich <arno-petrus.friedrich@informatik.hs-fulda.de>
2021-02-08 13:36:06 +0100

    #42 refactor: New constants for pattern and exception message

--- src/main/java/MultiMath_Math/NumeralSystems/OctalToDecimal.java
+++ src/main/java/MultiMath_Math/NumeralSystems/OctalToDecimal.java
@@ -5,10 +5,12 @@ public class OctalToDecimal {
     private static final int OCTAL_BASE = 8;
     private static final int DIVISION_MODULO_DIVISOR = 10;
     private static final String NEGATIVE_VALUE_EXCEPTION_MESSAGE = "No negative Values!";
+    private static final String NO_OCTAL_NUMBER_EXCEPTION_MESSAGE = "Not an octal number!";
+    private static final String OCTAL_PATTERN = "^[0-7]+";
 
     public int convert(int oct) throws Exception {
         if (oct < 0) throw new Exception(NEGATIVE_VALUE_EXCEPTION_MESSAGE);
-        if (!Integer.toString(oct).matches("^[0-7]+")) throw new Exception("Not an octal number!");
+        if (!Integer.toString(oct).matches(OCTAL_PATTERN)) throw new Exception(NO_OCTAL_NUMBER_EXCEPTION_MESSAGE);
         return calculateDecimalByOct(oct);
     }
 
