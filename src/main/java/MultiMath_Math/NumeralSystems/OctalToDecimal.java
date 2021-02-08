d677a3d2c51d9a6db92c1d841e9b72d860bc793b
arnofriedrich <arno-petrus.friedrich@informatik.hs-fulda.de>
2021-02-08 12:54:56 +0100

    #42 refactor: Introduced constants

--- src/main/java/MultiMath_Math/NumeralSystems/OctalToDecimal.java
+++ src/main/java/MultiMath_Math/NumeralSystems/OctalToDecimal.java
@@ -1,11 +1,15 @@
 package MultiMath_Math.NumeralSystems;
 
 public class OctalToDecimal {
+
+    private static final int OCTAL_BASE = 8;
+    private static final int DIVISION_MODULO_DIVISOR = 10;
+
     public int convert(int oct) {
-        int result = 0;
-        result += (oct % 10) * Math.pow(8, 0);
-        oct /= 10;
-        if (oct != 0) result += oct * Math.pow(8, 1);
-        return result;
+        int decimal = 0;
+        decimal += (oct % DIVISION_MODULO_DIVISOR) * Math.pow(OCTAL_BASE, 0);
+        oct /= DIVISION_MODULO_DIVISOR;
+        if (oct != 0) decimal += oct * Math.pow(OCTAL_BASE, 1);
+        return decimal;
     }
 }
